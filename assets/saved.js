5b7d14c2228090b483f3ee671960263019a5d37b
Merel Jacobs <merel.burleigh@gmail.com>
2023-06-27 10:36:39 +1000

    local storage printing to page

--- assets/saved.js
+++ assets/saved.js
@@ -1,20 +1,28 @@
-JSON.parse(localStorage.getItem('workout'));
-
 const savedWorkout = JSON.parse(localStorage.getItem("workout"));
 
-var printWorkout = document.createElement('div');
-document.innerHTML = printWorkout;
 
-var setInfoArray = []
 
-window.onload = function() {
-for (var i = 0; i < savedWorkout.length; i++) {
-    var title = savedWorkout[i].exerciseName;
-    var date = savedWorkout[i].date;
-    var setInfo = savedWorkout[i].allSetInfo;
-    setInfo.forEach(value => {
-        setInfoArray.push(value);
-    });
-    printWorkout.innerHTML += `<p>${title}</p><p>${date}</p><p>${setInfoArray}</p>`
-}
-}
+var setInfoArray = [];
+
+if (savedWorkout) {
+    var printWorkout = document.createElement('div');
+    printWorkout.classList.add('printedWorkout');
+    document.body.appendChild(printWorkout);
+    var thisWorkout = document.createElement('h2');
+    printWorkout.appendChild(thisWorkout);
+    thisWorkout.innerHTML += savedWorkout[0].date;
+  for (var i = 0; i < savedWorkout.length; i++) {
+    var title = savedWorkout[i].exerciseName; // Get the title from the saved workout
+    var date = savedWorkout[i].date; // Get the date from the saved workout
+    var setInfo = savedWorkout[i].allSetInfo; // Get the setInfo from the saved workout
+    // adds a space between print 
+    var setInfoString = setInfo.map(obj => obj).join(', ');
+
+    thisWorkout.innerHTML += `<h4 class="savedTitle">${title}</h4><p>${setInfoString}</p>`;
+  } 
+} else {
+    var printWorkout = document.createElement('div');
+    printWorkout.classList.add('printedWorkout');
+    document.body.appendChild(printWorkout); // Append the div to the document body 
+    printWorkout.innerHTML += `<p>None Saved.</p>`
+};
