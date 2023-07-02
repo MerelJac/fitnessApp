a9c2920873bfccca11d2c5a20514ca3707dcc730
Merel Jacobs <merel.burleigh@gmail.com>
2023-07-02 11:08:32 +1000

    debug saved page

--- assets/saved.js
+++ assets/saved.js
@@ -5,7 +5,37 @@ const randomSavedWorkout = JSON.parse(localStorage.getItem('randomWorkout'));
 
 var setInfoArray = [];
 
-if (savedWorkout) {
+if (savedWorkout && randomSavedWorkout) {
+
+  // saved workout
+  var printWorkout = document.createElement('div');
+    printWorkout.classList.add('printedWorkout');
+    document.body.appendChild(printWorkout);
+    var thisWorkout = document.createElement('h2');
+    printWorkout.appendChild(thisWorkout);
+    thisWorkout.innerHTML += savedWorkout[0].date;
+  for (var i = 0; i < savedWorkout.length; i++) {
+    var title = savedWorkout[i].exerciseName; // Get the title from the saved workout
+    var date = savedWorkout[i].date; // Get the date from the saved workout
+    var setInfo = savedWorkout[i].allSetInfo; // Get the setInfo from the saved workout
+    // adds a space between print 
+    var setInfoString = setInfo.map(obj => obj).join(', ');
+
+    thisWorkout.innerHTML += `<h4 class="savedTitle">${title}</h4><p>${setInfoString}</p>`;}
+
+    // random saved
+    var printWorkout = document.createElement('div');
+    printWorkout.classList.add('printedWorkout');
+    document.body.appendChild(printWorkout);
+    var thisWorkout = document.createElement('h2');
+    printWorkout.appendChild(thisWorkout);
+    thisWorkout.innerHTML += randomSavedWorkout[0];
+  for (var i = 0; i < randomSavedWorkout[1].length; i++) {
+    var exercise = randomSavedWorkout[1][i];
+    printWorkout.innerHTML += `<p class="savedTitle">${exercise}<p>`
+  } 
+
+} else if (savedWorkout) {
     var printWorkout = document.createElement('div');
     printWorkout.classList.add('printedWorkout');
     document.body.appendChild(printWorkout);
@@ -21,14 +51,7 @@ if (savedWorkout) {
 
     thisWorkout.innerHTML += `<h4 class="savedTitle">${title}</h4><p>${setInfoString}</p>`;
   } 
-} else {
-    var printWorkout = document.createElement('div');
-    printWorkout.classList.add('printedWorkout');
-    document.body.appendChild(printWorkout); // Append the div to the document body 
-    printWorkout.innerHTML += `<p>None Saved.</p>`
-};
-
-if (randomSavedWorkout) {
+} else if (randomSavedWorkout) {
   var printWorkout = document.createElement('div');
   printWorkout.classList.add('printedWorkout');
   document.body.appendChild(printWorkout);
